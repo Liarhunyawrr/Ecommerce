6709c8a97ab004f314ea675841e54c3fdb043df4
Liarhunyawrr <kamranprince5757@gmail.com>
2024-02-20 15:49:54 +0500

    changes

--- Server/Schema/Post.js
+++ Server/Schema/Post.js
@@ -1,11 +1,8 @@
-// mongoose.connect("mongodb://127.0.0.1:27017/store");
-//
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://LiarLio:7560294kpkp@cluster0.pbo2xii.mongodb.net/?retryWrites=true&w=majority"
+  "mongodb+srv://LiarLio:7560294kpkp@cluster0.v8vkaxv.mongodb.net/?retryWrites=true&w=majority"
 );
-
 const postdets = mongoose.Schema({
   id: Number,
   title: String,
